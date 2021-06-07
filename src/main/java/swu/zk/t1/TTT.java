7bc5f6754ddfa3c266e37d1bca4ce1c6e5e5be71
zk <982847942@qq.com>
2021-06-07 11:51:30 +0800

    1

---
+++ src/main/java/swu/zk/t1/TTT.java
@@ -0,0 +1,13 @@
+package swu.zk.t1;
+
+/**
+ * @Classname TTT
+ * @Description TODO
+ * @Date 2021/6/7 11:46
+ * @Created by brain
+ */
+public class TTT {
+    public static void main(String[] args) {
+        System.out.println("hahahha");
+    }
+}
